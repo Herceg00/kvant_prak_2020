bb1df76952b17c50f3c171fa64e40787b5950cb1
Dmitry <53238312+Herceg00@users.noreply.github.com>
2020-02-26 00:52:56 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ complexd* generate_condition(int n){
     complexd *V = new complexd[qsize];
     double time_init = time(NULL);
     double module =0;
-#pragma omp parallel shared(V,time_init) reduction(+: module)
+#pragma omp parallel shared(V) firstprivate(time_init) reduction(+: module)
     {
         unsigned int seed = omp_get_thread_num() * (unsigned)time_init;
 #pragma omp for schedule(static)
@@ -48,17 +48,14 @@ void OneQubitEvolution(complexd* V,complexd* W,complexd U[2][2],int n, int k){
     int pow=1<<(shift);
 
     int N=1<<n;
-#pragma omp parallel shared(pow,shift,N)
+#pragma omp parallel firstprivate(pow,shift,N)
     {
+
 #pragma omp for schedule(static)
         for (int i = 0; i < N; i++) {
             int i0 = i & ~pow;
-
-
             int i1 = i | pow;
-
             int iq = (i & pow) >> shift;
-
             W[i] = U[iq][0] * V[i0] + U[iq][1] * V[i1];
         }
     }
@@ -66,18 +63,14 @@ void OneQubitEvolution(complexd* V,complexd* W,complexd U[2][2],int n, int k){
 }
 
 int main(int argc , char** argv) {
-    if(argc<4){
+    if(argc<3){
         cout<<"not enough arguments";
         return 1;
     }
-    omp_set_num_threads(atoi(argv[3]));
     int n = atoi(argv[1]); // number of qubits
     int k = atoi(argv[2]); // operated qubit number
     assert(n>=k);
     struct timeval start,stop;
-
-    //printf("%d",omp_get_num_threads());
-
     complexd *V = generate_condition(n);
     unsigned long long index = 1LLU<<n;
     complexd *W = new complexd[index];
@@ -88,10 +81,9 @@ int main(int argc , char** argv) {
     U[1][1] =  - 1/sqrt(2);
     gettimeofday(&start,NULL);
     OneQubitEvolution(V,W,U,n,k);
-
     gettimeofday(&stop,NULL);
-
-    printf("LIFETIME:%lf s\n",(float)((stop.tv_sec - start.tv_sec)*1000000 + stop.tv_usec - start.tv_usec)/1000000);
+    cout << '~' << n << '\t' << k << '\t';
+    printf(" LIFETIME:%lf s ",(float)((stop.tv_sec - start.tv_sec)*1000000 + stop.tv_usec - start.tv_usec)/1000000);
     delete [] V;
     delete [] W;
 
